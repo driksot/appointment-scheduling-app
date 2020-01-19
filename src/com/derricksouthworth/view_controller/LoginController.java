b221ee54711c9275a3b6a3c159fdd5a8633c6a4b
Derrick Southworth <derricksouthworth@gmail.com>
2020-01-19 12:39:48 -0700

    Update LoginController skeleton

--- src/com/derricksouthworth/view_controller/LoginController.java
+++ src/com/derricksouthworth/view_controller/LoginController.java
@@ -1,9 +1,40 @@
 package com.derricksouthworth.view_controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 /**
  *
  * @author derrick.southworth
  */
 
 public class LoginController {
+
+    @FXML
+    private Label lblLogInTitle;
+
+    @FXML
+    private Label lblUserName;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    private Label lblPassword;
+
+    @FXML
+    private PasswordField pwdPassword;
+
+    @FXML
+    private Button btnSignIn;
+
+    @FXML
+    void submitSignIn(ActionEvent event) {
+
+    }
+
 }
